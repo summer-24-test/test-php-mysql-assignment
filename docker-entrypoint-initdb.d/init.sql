2602491b3e2822a2fe096f33cd5c564f7336f1b0
Jeremy McWilliams <jeremym@lclark.edu>
2024-08-02 23:30:10 +0000

    update sql files

---
+++ docker-entrypoint-initdb.d/init.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE IF NOT EXISTS my_database;
+CREATE USER IF NOT EXISTS 'my_user'@'%' IDENTIFIED BY 'my_password';
+GRANT ALL PRIVILEGES ON my_database.* TO 'my_user'@'%';
+FLUSH PRIVILEGES;
